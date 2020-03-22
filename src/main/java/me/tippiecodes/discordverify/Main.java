ada99df5740618aea2820f7c6bedac27e6389161
Red-Dev-Red <rowanmtvanrooijen@gmail.com>
2020-03-22 02:59:33 +0100

    Added ping-pong discord protocol

--- src/main/java/me/tippiecodes/discordverify/Main.java
+++ src/main/java/me/tippiecodes/discordverify/Main.java
@@ -1,10 +1,27 @@
 package me.tippiecodes.discordverify;
 
+import me.tippiecodes.discordverify.listeners.DiscordChatMessage;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import javax.security.auth.login.LoginException;
+
 public class Main extends JavaPlugin {
+
+    private Main plugin;
+
     @Override
     public void onLoad() {
-
+        saveDefaultConfig();
     }
+
+    public void main(String[] arguments)
+            throws LoginException, InterruptedException
+    {
+        JDA api = JDABuilder.createDefault(this.getConfig().getString("bot.token"))
+                .addEventListeners(new DiscordChatMessage())
+                .build().awaitReady();
+    }
+
 }
---
+++ src/main/java/me/tippiecodes/discordverify/utils/Utils.java
@@ -0,0 +1,10 @@
+package me.tippiecodes.discordverify.utils;
+
+import org.bukkit.ChatColor;
+
+public class Utils {
+
+    public static String chat (String s) {
+        return ChatColor.translateAlternateColorCodes('&', s);
+    }
+}
